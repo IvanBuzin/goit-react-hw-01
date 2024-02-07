3f124f72d1cf5088c378892f1f44ce39bda01c01
IvanBuzin <ua.buzin@gmail.com>
2024-02-07 21:17:23 +0200

    x

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,31 @@
+const data = {
+  id: "1",
+  cardNumber: "1234 5678 9012 3456",
+  cardType: "Visa",
+  cardOwner: "John Doe",
+  date: "2024-01-01",
+  amount: 100,
+  description: "Payment for groceries",
+};
 
+const Payment = () => {
+  return (
+    <div>
+      <p>Amount: </p>
+      <p>Status: Paid or Pending</p>
+      <p>Description:</p>
+      <p>Card Nunber:</p>
+      <p>Card Type:</p>
+      <p>Card Holder Name:</p>
+      <p>Payment Date:</p>
+    </div>
+  );
+};
 
 export const App = () => {
-  const username = "mango";
-
   return (
     <>
-      <p>Hello { username}</p>
-      <h1>Vite + React</h1>
-      <button> Click me</button>
+      <Payment />
     </>
-  )
-}
-
-
+  );
+};
