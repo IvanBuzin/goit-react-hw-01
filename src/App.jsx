71fa2b5813b044c79daf7f402dc61fac02a2e3e2
IvanBuzin <ua.buzin@gmail.com>
2024-02-07 20:34:30 +0200

    c

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,15 @@
-import './App.css'
 
-function App() {
+
+export const App = () => {
+  const username = "mango";
+
   return (
     <>
+      <p>Hello { username}</p>
       <h1>Vite + React</h1>
       <button> Click me</button>
     </>
   )
 }
 
-export default App
+
