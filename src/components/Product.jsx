12464a11ebbad7ebf02d583267375b1a58f07349
IvanBuzin <ua.buzin@gmail.com>
2024-02-09 21:59:12 +0200

    ff

--- src/components/Product.jsx
+++ src/components/Product.jsx
@@ -1,15 +1,11 @@
 // src/components/Product.jsx
 
-export const Product = (props) => {
+export const Product = ({ name, imgUrl, price }) => {
   return (
     <div>
-      <h2>{props.name}</h2>
-      <img
-        src="<https://images.pexels.com/photos/461198/pexels-photo-461198.jpeg?dpr=2&h=480&w=640>"
-        alt="Tacos With Lime"
-        width="480"
-      />
-      <p>Price: 999 credits</p>
+      <h2>{name}</h2>
+      <img src={imgUrl} alt={name} width="480" />
+      <p>Price: {price} credits</p>
     </div>
   );
 };
