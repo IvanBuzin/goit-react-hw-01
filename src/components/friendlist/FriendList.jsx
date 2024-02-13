521165ef1eab97134b6943a2d00efbb76b99b5c4
IvanBuzin <ua.buzin@gmail.com>
2024-02-13 22:02:26 +0200

    g

--- src/components/friendlist/FriendList.jsx
+++ src/components/friendlist/FriendList.jsx
@@ -1,5 +1,5 @@
 import style from "./FriendList.module.css";
-import { FriendListItem } from "../friendlistItem/FriendListItem";
+import { FriendListItem } from "./friendlistItem/FriendListItem";
 
 export const FriendList = ({ friends }) => {
   return (
